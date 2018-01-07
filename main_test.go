1d02bd426759e509ea7e90b5149e8413d1cbaf44
caeost <caeost@gmail.com>
2018-01-06 19:19:04 -0800

    testing with more scale

--- main_test.go
+++ main_test.go
@@ -8,18 +8,20 @@ func TestCaseA(t *testing.T) {
 	Register("a", "Y", []string{"a", "b", "c"})
 
 	if err := Generate("a"); err != nil {
-		t.Error("Could not generate")
+		t.Error("Could not generate", err)
 	}
 
-	instance := make(map[string]string)
+	var hash string
+	for i := 0; i < 1000; i++ {
+		var err error
+		hash, err = Store("a", generateInst())
 
-	instance["X"] = "true"
-	instance["Y"] = "b"
-	hash, err := Store("a", instance)
-
-	if err != nil {
-		t.Error("could not Store")
+		if err != nil {
+			t.Error("could not Store", err)
+		}
 	}
+
+	t.Log("hash: " + hash)
 
 	str, err := Get(hash)
 
@@ -28,4 +30,13 @@ func TestCaseA(t *testing.T) {
 	}
 
 	t.Log(str)
+}
+
+func generateInst() Instance {
+	instance := make(Instance)
+
+	instance["X"] = "true"
+	instance["Y"] = "b"
+
+	return instance
 }
